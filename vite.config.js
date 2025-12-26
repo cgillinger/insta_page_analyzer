20d81c232e2dd65a3a62df4cdaf794c7d93c7674
Christian Gillinger <christian.gillinger@gmail.com>
2025-12-26 22:15:59 +0100

    Fix: Correct base path to match repo name (understreck)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/insta-page-analyzer/',
+  base: '/insta_page_analyzer/',
   plugins: [react()],
   resolve: {
     alias: {
